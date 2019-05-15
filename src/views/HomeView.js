cc7e81bdadf3b164a9161e996ad4eb1bb137565e
eenriquez1 <edgarg.enriquez@gmail.com>
2019-05-15 17:07:37 -0400

    Updated home view

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -6,7 +6,7 @@ class HomeView extends Component {
         <div>
           <h1 className="text-center">Welcome to Cheese React</h1>
             <h2 className="text-center">Below are the links in order to create your first cheese!</h2>
-                <ul className="text-center">
+                <ul className="text-center" list-style-position="inside">
                  <li><a href="/cheeses">Cheeses</a></li>
                  <li><a href="/menus">Menus</a></li>
                  <li><a href="/categories">Categories</a></li>
