840e17c15aa1e7eb8a68e39af753657705325c4c
eenriquez1 <edgarg.enriquez@gmail.com>
2019-05-15 18:48:40 -0400

    Update 1.1

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -3,17 +3,17 @@ import React, { Component } from "react";
 class HomeView extends Component {
   render() {
     return (
+        
         <div>
-          <h1 className="text-center">Welcome to Cheese React</h1>
-            <h2 className="text-center">Below are the links in order to create your first cheese!</h2>
-                <ul max-width="1%" margin="auto"  text-align="left">
-                 <li><a href="/cheeses">Cheeses</a></li>
-                 <li><a href="/menus">Menus</a></li>
-                 <li><a href="/categories">Categories</a></li>
+        
+          <h1>Welcome to Cheese React</h1>
+            <h2>Below are the links in order to create your first cheese!</h2>
+                <ul className="header">
+                    <li><a href="/menus">Menus</a></li>
+                    <li><a href="/cheeses">Cheeses</a></li>
+                    <li><a href="/categories">Categories</a></li>
                 </ul>
-        <div className="content">
-             
-          </div>
+          
         </div>
     );
   }
